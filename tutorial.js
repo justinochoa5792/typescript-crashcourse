90417fb5dc7d6c8ad976b694514ef7a4f54ce23f
Justin Ochoa <just.och92@gmail.com>
2024-06-02 10:52:08 -0400

    finished tutorial

--- tutorial.js
+++ tutorial.js
@@ -1,18 +1,61 @@
 "use strict";
-// let id: number = 5;
-// let company: string = "Acme Corp";
-// let isPublished: boolean = true;
-const User = {
-    id: 2,
-    name: "Pedro",
-    greet(message) {
-        console.log(message);
-    },
-};
-if (!User.age) {
-    console.log("No Age of User");
+// // let id: number = 5;
+// // let company: string = "Acme Corp";
+// // let isPublished: boolean = true;
+// // let ids: number[] = [1, 2, 3, 4, 5];
+// // let x: any = "Justin";
+// // let xArr: any[] = ["justin", 0, false];
+// // const concatValues = (a: string, b: string) => {
+// //   return a + b;
+// // };
+// // console.log(concatValues("hello", "world"));
+// // console.log(concatValues("5", "10"));
+// interface UserInterFace {
+//   id: number;
+//   name: string;
+//   age?: number;
+//   greet(message: string): void;
+// }
+// const User: UserInterFace = {
+//   id: 2,
+//   name: "Pedro",
+//   greet(message) {
+//     console.log(message);
+//   },
+// };
+// if (!User.age) {
+//   console.log("No Age of User");
+// } else {
+//   console.log(User.age);
+// }
+// User.greet("Hello!");
+// const printID = (id: string | number) => {
+//   console.log(`ID:${id}`);
+// };
+// printID("justin");
+// enum LoginError {
+//   Unauthorized = "Unauthorized",
+//   NoUser = "No User",
+//   Internal = "Internal",
+// }
+// const printErrorMsg = (error: LoginError) => {
+//   if (error == LoginError.Unauthorized) {
+//     console.log("User not authed");
+//   }
+// };
+// printErrorMsg(LoginError.Unauthorized);
+class StorageContainer {
+    constructor() {
+        this.contents = [];
+    }
+    addItem(item) {
+        this.contents.push(item);
+    }
+    getItem(idx) {
+        return this.contents[idx];
+    }
 }
-else {
-    console.log(User.age);
-}
-User.greet("Hello!");
+const usernames = new StorageContainer();
+usernames.addItem("justin");
+usernames.addItem("developer");
+console.log(usernames.getItem(0));
--- tutorial.ts
+++ tutorial.ts
@@ -1,37 +1,79 @@
-// let id: number = 5;
-// let company: string = "Acme Corp";
-// let isPublished: boolean = true;
+// // let id: number = 5;
+// // let company: string = "Acme Corp";
+// // let isPublished: boolean = true;
 
-// let ids: number[] = [1, 2, 3, 4, 5];
-// let x: any = "Justin";
-// let xArr: any[] = ["justin", 0, false];
+// // let ids: number[] = [1, 2, 3, 4, 5];
+// // let x: any = "Justin";
+// // let xArr: any[] = ["justin", 0, false];
 
-// const concatValues = (a: string, b: string) => {
-//   return a + b;
+// // const concatValues = (a: string, b: string) => {
+// //   return a + b;
+// // };
+
+// // console.log(concatValues("hello", "world"));
+// // console.log(concatValues("5", "10"));
+
+// interface UserInterFace {
+//   id: number;
+//   name: string;
+//   age?: number;
+//   greet(message: string): void;
+// }
+
+// const User: UserInterFace = {
+//   id: 2,
+//   name: "Pedro",
+//   greet(message) {
+//     console.log(message);
+//   },
 // };
 
-// console.log(concatValues("hello", "world"));
-// console.log(concatValues("5", "10"));
+// if (!User.age) {
+//   console.log("No Age of User");
+// } else {
+//   console.log(User.age);
+// }
 
-interface UserInterFace {
-  id: number;
-  name: string;
-  age?: number;
-  greet(message: string): void;
+// User.greet("Hello!");
+
+// const printID = (id: string | number) => {
+//   console.log(`ID:${id}`);
+// };
+
+// printID("justin");
+
+// enum LoginError {
+//   Unauthorized = "Unauthorized",
+//   NoUser = "No User",
+//   Internal = "Internal",
+// }
+
+// const printErrorMsg = (error: LoginError) => {
+//   if (error == LoginError.Unauthorized) {
+//     console.log("User not authed");
+//   }
+// };
+
+// printErrorMsg(LoginError.Unauthorized);
+
+class StorageContainer<T> {
+  private contents: T[];
+
+  constructor() {
+    this.contents = [];
+  }
+
+  addItem(item: T): void {
+    this.contents.push(item);
+  }
+
+  getItem(idx: number): T | undefined {
+    return this.contents[idx];
+  }
 }
 
-const User: UserInterFace = {
-  id: 2,
-  name: "Pedro",
-  greet(message) {
-    console.log(message);
-  },
-};
+const usernames = new StorageContainer<string>();
+usernames.addItem("justin");
+usernames.addItem("developer");
 
-if (!User.age) {
-  console.log("No Age of User");
-} else {
-  console.log(User.age);
-}
-
-User.greet("Hello!");
+console.log(usernames.getItem(0));
